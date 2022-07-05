165019e199eee19803eb3aed9001e89469d72790
uper23 <xxxnaujxxx@gmail.com>
2022-07-05 18:38:20 -0300

    cambios parte 2

--- js/script.js
+++ js/script.js
@@ -7,7 +7,23 @@
         const number = document.querySelector('#numero');
         const number2 = document.querySelector('#numero2');
         const resultado = document.querySelector('#resultado');
+        const sumar = document.querySelector('#sumar');
+        const restar = document.querySelector('#restar');
+        const multiplicar = document.querySelector('#multiplicar');
+        const dividir = document.querySelector('#dividir');
+
+        sumar.addEventListener('click', calcular1);
+
+        function calcular1(event) {
+            event.preventDefault();
+            const valor1 = number.value,
+                  valor2 = number2.value
+            
+
+            const resultado = (valor1 + valor2);
+
+            console.log(resultado)
+        }
     });
 
-}())
-console.log(numero);
+}());
