0c3930a93646cbda001eb76402d874667d2fe8c8
uper23 <xxxnaujxxx@gmail.com>
2022-07-05 04:55:40 -0300

    cambios parte 1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// ejercicios
+
+(function(){
+    "use strict";
+
+    document.addEventListener('DOMContentLoaded', function(){
+        const number = document.querySelector('#numero');
+        const number2 = document.querySelector('#numero2');
+        const resultado = document.querySelector('#resultado');
+    });
+
+}())
+console.log(numero);
